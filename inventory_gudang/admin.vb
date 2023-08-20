b198249be689cb177ac64f01e1d6c7004c830b13
HafidzDwiRahmansyah <dwihafidz009@gmail.com>
2023-08-20 15:40:56 +0700

    aplikasi inventory gudang vb

---
+++ inventory_gudang/admin.vb
@@ -0,0 +1,10 @@
+﻿Public Class admin
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub admin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
